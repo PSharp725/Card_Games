e9f40196641d804c20064498ef6d67dcad07f05c
PSharp725 <Patricksharp725@gmail.com>
2023-04-30 15:52:17 -0600

    clean up changes

--- solitaire.cpp
+++ solitaire.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 enum Rank {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
-enum Suit {SPADES, HEARTS, DIAMONDS, CLUBS};
+enum Suit {SPADES, CLUBS, HEARTS, DIAMONDS};
 
 struct Card
 {
